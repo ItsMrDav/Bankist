77acaa3e58e20f8e79032656160035ef577c0219
ItsMrDav <davutt.simsek@gmail.com>
2023-04-20 18:52:52 +0200

    basic display funcs

--- script.js
+++ script.js
@@ -64,6 +64,8 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // *****************************************************************
 // ************* GENERAL *******************************************
 // *****************************************************************
+
+// DISPLAYING MOVEMENTS===========================================
 const displayMovements = function (movements) {
   containerMovements.innerHTML = '';
   movements.forEach(function (mov, i) {
@@ -83,16 +85,119 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+// DISPLAYING BALANCE===========================================
+const calcDisplayPrintBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayPrintBalance(account1.movements);
+
+// CREATING USER NAMES===========================================
+const createUserNames = function (accs) {
+  accs.forEach(acc => {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUserNames(accounts);
+
+// *****************************************************************
+// ************* EXERCISES *****************************************
+// *****************************************************************
 /*
+// ***** CODING CHALLANGE #1 *********************************************
+const dogsJulia1 = [3, 5, 2, 12, 7];
+const dogsKate1 = [4, 1, 15, 8, 3];
+const dogsJulia2 = [9, 16, 6, 8, 3];
+const dogsKate2 = [10, 5, 6, 1, 4];
+
+const checkDogs = (arr1, arr2) => {
+  const correctedArr1 = arr1.slice(1, -2);
+  const allDogs = correctedArr1.concat(arr2);
+  allDogs.forEach((age, i, arr) => {
+    age >= 3
+    ? console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`)
+    : console.log(`Dog number ${i + 1} is still a puppy is ${age} years old`);
+  });
+};
+
+checkDogs(dogsJulia1, dogsKate1);
+console.log(`-------DATA SET 2--------`);
+checkDogs([9, 16, 6, 8, 3], dogsKate2);
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+
+const movementsUsd = movements.map(mov => Math.trunc(mov * eurToUsd));
+
+console.log(movements);
+console.log(movementsUsd);
+
+const movementsUsdFor = [];
+for (const mov of movements) {
+  movementsUsdFor.push(Math.trunc(mov * eurToUsd));
+}
+console.log(movementsUsdFor);
+
+const movementDescriptions = movements.map(
+  (mov, i) =>
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+    mov
+    )}`
+    );
+    console.log(movementDescriptions);
+    
 
 
+// FILTER METHOD
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(mov => mov > 0);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
 
 
+// REDUCE METHOD
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+console.log(movements);
+
+// accumulator -> SNOWBALL
+const balance = movements.reduce((acc, cur, i, arr) => acc + cur, 100);
+console.log(balance);
+
+let balance2 = 100;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce(
+  (acc, mov) => (acc > mov ? acc : mov),
+  movements[0]
+);
+console.log(max);
+console.log(Math.max(...movements));
 
 
+// CODING CHALLANGE #2
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+const data2 = [16, 6, 10, 5, 6, 1, 4];
 
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
-
-
-
+  return humanAges.toFixed(1);
+};
+console.log(calcAverageHumanAge(data1));
+console.log(calcAverageHumanAge(data2));
 */
